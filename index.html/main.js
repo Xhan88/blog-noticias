b8266f722501da2d47f9f546c24c67a575fd9ca9
Xhan88 <101303440+Xhan88@users.noreply.github.com>
2022-06-13 22:58:13 -0500

    Add files via upload

---
+++ index.html/main.js
@@ -0,0 +1,9 @@
+document.querySelector('.menu-btn').addEventListener('click', ()=>{
+document.querySelector('.nav-menu').classList.toggle('show');
+});
+
+
+ScrollReveal().reveal('.showcase');
+ScrollReveal().reveal('.news-cards', {delay: 500});
+ScrollReveal().reveal('.cards-baner-one', {delay: 500});
+ScrollReveal().reveal('.cards-baner-two', {delay: 500});
